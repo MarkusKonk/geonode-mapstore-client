f6071c781b3b8de1e75e3741d407111d87cadee2
MarkusKonk <m.konkol@52north.org>
2022-10-21 15:55:49 +0200

    add marker to map

--- geonode_mapstore_client/client/js/plugins/StaHandler.jsx
+++ geonode_mapstore_client/client/js/plugins/StaHandler.jsx
@@ -1,51 +1,38 @@
-/*
-Map.jsx einladen
-function HandlerSTa extends Map? //irgendwie an die props rankommen, wo die layer drin sind. Oder über den reducer?
-dann layer durchgucken und checken ob STA layer dabei ist
-wenn dem so ist, dann marker hinzufügen mit Koordinaten aus map.center
-
-gucken, wie die featrures in den state kommen
-da muss der marker rein
-dann sollte der state aktualisieren und so auch die map
-*/
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { createPlugin } from '../../MapStore2/web/client/utils/PluginsUtils';
-import map from '@mapstore/framework/plugins/Map';
 import { connect } from 'react-redux';
-//import layersReducer from "@mapstore/framework/reducers/layers";
-import {get} from 'lodash';
-import PropTypes from 'prop-types';
-import { createSTA_features } from './staHandler/actions_StaHanlder';
-import sta_add_features from './staHandler/reducers_StaHandler';
-import { createStore } from 'redux';
 import {staLayerSelector} from './staHandler/selector_StaHandler';
 import {getMarkerLayer} from '@mapstore/framework/utils/MapInfoUtils';
 import layers from '@mapstore/framework/reducers/layers';
 import {addLayer} from '@mapstore/framework/actions/layers';
 
 function STA(props) {
-    
-    console.log("sta layer stuff")
-    
-    if (props[0]) {
-        const marker = getMarkerLayer("GetFeatureInfo", {lat:50.000000, lng:-98.525390625});
-        props.add_sta_layer(marker);
+    const [value, setValue] = useState('')
+    if (props.staLayers.length > 0) {
+        const marker = createMarkerLayer(props.staLayers[0]);
+        useEffect(() => {
+            props.addLayerAction(marker)
+        }, [value]);
     }
-
-    return (
-        <div></div>
-    )
+    return(
+    <div></div>    
+    );
 }
 
-const StaPlugin = connect(staLayerSelector, {
-    //sta_features: createSTA_features,
-    add_sta_layer: addLayer,
+const createMarkerLayer = (staLayer) => {
+    return getMarkerLayer("GetFeatureInfo", {lat:staLayer.bbox.bounds.maxy, lng:staLayer.bbox.bounds.maxx});
+}
+
+const StaPlugin = connect((state) => ({
+    staLayers: staLayerSelector(state)
+}), {
+    addLayerAction: addLayer,
 }
 )(STA)
 
 export default createPlugin('StaHandler', {
     component: StaPlugin,
     reducers: {
-        layers
+        addLayerReducer: layers,
     }
 });
--- geonode_mapstore_client/client/js/plugins/staHandler/actions_StaHanlder.js
+++
@@ -1,8 +0,0 @@
-export const CREATE_STA_FEATURES = 'CREATE_STA_FEATURES';
-
-export function createSTA_features(payload) {
-    return {
-        type: "CREATE_STA_FEATURES",
-        payload
-    };
-}
